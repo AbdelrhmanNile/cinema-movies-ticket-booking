0d242ab34039f2eea0c72e12604d38a63bebe5dd
Mohamed2251 <m7mdflash20242@gmail.com>
2021-12-21 14:42:01 +0200

    fix

--- mycinema/src/main/java/com/it/mycinema/Seat.java
+++ mycinema/src/main/java/com/it/mycinema/Seat.java
@@ -1,4 +1,22 @@
 package com.it.mycinema;
 public class Seat {
+          private boolean taken;
+        public Seat (){  
         
+            this.taken = false ;
+        }    
+         public String state (){
+             if(taken==false){
+                 return "free";
+             } 
+             else{
+                 return "taken";
+}
+    }
+         public void reserve(){
+             taken=true;
+         }
+         public void reclaim() {
+             taken=false;
+         }
 }
