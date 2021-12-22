4acc38119143d7ea35013d319f232ffd16bf5e3a
AbdelrhmanNile <abdelrhmannile@gmail.com>
2021-12-22 20:27:17 +0200

    fixing conflict

--- mycinema/src/main/java/com/it/mycinema/Seat.java
+++ mycinema/src/main/java/com/it/mycinema/Seat.java
@@ -1,43 +1,4 @@
 package com.it.mycinema;
 public class Seat {
-    private char section;
-    private int index;
-    private Boolean taken;
-
-    public Seat(char section, int index){ // contructor
-        this.section = section;
-        this.index = index;
-        this.taken = false;
-         
-    }
-
-    // method to check the state of a seat, taken or not
-    public Boolean state(){
-        if (this.taken == false){
-            return false;
-        }
-        else
-            return true;
-    }
-
-    // method reserves a seat, change taken value to true
-    public String reserve(){
-        if(this.taken == true){ // if you are tring to reserve an already taken seat
-            return "Sorry, this seat is alrerady taken";
-        }
-        else{
-            this.taken = true;
-            return "";
-        }
-    }
-
-    public void reclaim(){ // frees a taken seat
-        this.taken = false;
-    }
-    
-
-    public String toString(){
-        return "{" + section + index + "}";
-    }
-    
+  
 }
